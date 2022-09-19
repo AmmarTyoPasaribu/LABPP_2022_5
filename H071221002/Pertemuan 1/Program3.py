f9ebc8b7ffafc66436746dde16a65c31c7715dfb
elvatimang15 <elvaaprilitimang15@gmail.com>
2022-09-19 07:08:39 -0700

    Pratikum 1 Ardika

---
+++ H071221002/Pertemuan 1/Program3.py
@@ -0,0 +1,17 @@
+import math
+jari_jari_alas = int(input('Masukkan Jari-jari : '))
+tinggi = int(input('Masukkan Tinggi : '))
+
+r=jari_jari_alas
+t=tinggi
+
+phi = math.pi
+s = (r*r)+(t*t)
+S = s**0.5
+
+volume = (phi*r*r*t)/3
+luas_permukaan = (phi*r*S)+phi*r*r
+
+print('Volume : ', round(volume,2))
+print('Luas Permukaan : ', 
+round(luas_permukaan,2))
