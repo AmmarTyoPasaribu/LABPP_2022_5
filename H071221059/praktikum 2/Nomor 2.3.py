d6e82ff9c96e9e0eb37a39b73e80d57716b6da9b
zoelramadhan <zoelramadhan123@gmail.com>
2022-09-26 13:01:55 +0800

    praktikum dua

---
+++ H071221059/praktikum 2/Nomor 2.3.py
@@ -0,0 +1,13 @@
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+
+if a > b:
+    if a > c:
+        print('>', a, 'adalah nilai terbesar' )
+if b > a:
+    if b > c:
+        print('>', b, 'adalah nilai terbesar' )
+if c > a:
+    if c > b:
+        print('>', c, 'adalah nilai terbesar' )
